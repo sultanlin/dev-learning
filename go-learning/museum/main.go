0f22c6fe01dd238e1fbdfc36985bd7ad43b0acdc
Sultan Linjawi <skorpion219@hotmail.co.uk>
2024-01-08 04:13:42 +0300

    go website

--- go-learning/museum/main.go
+++ go-learning/museum/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
@@ -9,9 +10,20 @@ func handleHello(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from a Go program\n"))
 }
 
+func handleTemplate(w http.ResponseWriter, r *http.Request) {
+	html, err := template.ParseFiles("templates/index.tmpl")
+	if err != nil {
+		w.Write([]byte("Internal server error"))
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	html.Execute(w, "Test")
+}
+
 func main() {
 	server := http.NewServeMux()
 	server.HandleFunc("/hello", handleHello)
+	server.HandleFunc("/template", handleTemplate)
 
 	fs := http.FileServer(http.Dir("./public"))
 	server.Handle("/", fs)
